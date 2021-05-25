1496d14cb0fd2f530fb180eb7f7f048c0e6ef503
JoeBraybrook <joe.braybrook@pecometer.co.uk>
2021-05-25 16:21:30 +0000

    Angular Update - bug fixes (Need Grid Layout + Movement)

---
+++ mazegame/src/app/game/game-config/settings.ts
@@ -0,0 +1,8 @@
+export const XBLOCKS = 50;
+export const YBLOCKS = 50;
+export const BLOCK_SIZE = 10;
+export const DUNGEON_ONE = {
+
+
+
+}
--- mazegame/src/app/game/game.component.ts
+++ mazegame/src/app/game/game.component.ts
@@ -1,33 +1,80 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { BLOCK_SIZE, XBLOCKS, YBLOCKS } from '../game/game-config/settings';
+
+export interface IScreen {
+  drawScreen: (dungeon: number) => void;
+}
+
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
+
+
+
+
 export class GameComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  constructor(){}
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+private _ctx: CanvasRenderingContext2D
+  public dungeon: number;
+  public gameStarted: boolean = false;
+  public gameEnded: boolean = false;
+
+  public screen: IScreen;
+
+  @ViewChild('game', { static: true })
+    public set canvas(ele: ElementRef<HTMLCanvasElement>) {
+        if (ele) {
+            this._initScreen(ele.nativeElement);
+        }
+    }
+
+    @HostListener('window:keydown', ['$event'])
+    async keyEvent(event: KeyboardEvent): Promise<void> {
+      this._ctx.clearRect(0, 0, this._ctx.canvas.width, this._ctx.canvas.height);
+    }
+
+
+  
+
+ 
+
+  private _initScreen(canvas: HTMLCanvasElement): void {
+
+      this._ctx = canvas.getContext('2d');
+
+      // Here we calculate the size of the game screen using our predefined parameters in game-config/settings.ts
+      this._ctx.canvas.height = YBLOCKS * BLOCK_SIZE;
+      this._ctx.canvas.width = XBLOCKS * BLOCK_SIZE;
+
+      
+  }
+
+
+//   public drawBoard(dungeon: number): void {
+//     if (dungeon === 1) {
+       
+            
+//         }
+//       }
+
+  
+
+
+//   public newGame(): void {
+ 
+//     this.dungeon = 1;
+//     this.screen = new Screen(this._ctx);
+//     this._ctx.clearRect(0, 0, this._ctx.canvas.width, this._ctx.canvas.height);
+//     this.screen.drawScreen(this.dungeon);
+
+// }
+ 
+
 }
+
+
